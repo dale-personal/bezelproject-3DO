f6e8f61153c14631ff395bd7a27670dc345a7558
Dale Koch <dalek@marvin.com>
2022-04-16 20:40:49 -0500

    change to chd

--- rename-config-files.ps1
+++ rename-config-files.ps1
@@ -2,6 +2,6 @@ $files = Get-ChildItem -File -Filter *.cfg -Path "retroarch\config\4DO\"
 
 for ($i=0; $i -lt $files.Count; $i++) {
     Write-Output $files[$i].FullName
-    $new_name = $files[$i].BaseName + ".zip" + $files[$i].Extension
+    $new_name = $files[$i].BaseName + ".chd" + $files[$i].Extension
     Rename-Item -Path $files[$i].FullName -NewName $new_name
 }
